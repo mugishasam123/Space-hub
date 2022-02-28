8887b4614504ba1aeaab4efe1946ad536946f5f9
mugisha Samuel <mugishasamuel42@gmail.com>
2022-02-28 06:43:15 -0800

    Add profile navlink

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
+    
+      <Router>
       <NavBar />
+        <Routes>
+          <Route path="/" element={<h1>hello world</h1>}/>
+          <Route path="/profile" element={
+            <h1>hello pro</h1>
+          }/>
+        </Routes>
+      </Router>
     </>
   );
 }
