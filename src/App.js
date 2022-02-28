bc7efef7f5a0ee40645aaf27282caa8425507e03
mugisha Samuel <mugishasamuel42@gmail.com>
2022-02-28 09:16:19 -0800

    Add rocket structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
-    
       <Router>
-      <NavBar />
+        <NavBar />
         <Routes>
-          <Route path="/" element={<h1>hello world</h1>}/>
-          <Route path="/profile" element={
-            <h1>hello pro</h1>
-          }/>
-        </Routes>
-      </Router>
+          <Route path="/" element={<h1>homepage </h1>} />
+          <Route path="/Rockets" element={<h1> rockets</h1>} />
+          <Route path="/Missions" element={<h1> Missions</h1>} />
+          <Route path="/Profile" element={<h1> my profile </h1>} />{" "}
+        </Routes>{" "}
+      </Router>{" "}
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from './logo.png'
-import './styles/navbar.css'
+import logo from "./logo.png";
+import "./styles/navbar.css";
 const NavBar = () => {
   return (
     <>
       <div className="navbar">
+        <div className="logo-section">
         <img src={logo} alt="" className="logo" />
-        <NavLink to="/profile">My Profile</NavLink>
+        <h1>Space Travelers' Hub</h1>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/Rockets" >Rockets</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Missions">Missions</NavLink>
+          </li>
+          <hr  />
+          <li>
+            <NavLink to="/profile">My Profile</NavLink>
+          </li>
+        </ul>
       </div>
+      <hr style={{margin:'0 30px 0 30px'}} />
     </>
   );
 };
