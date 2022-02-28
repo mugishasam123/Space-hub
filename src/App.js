f3598f4327342cf6375bb2cf59cd2fd93adb0824
joskalenda <joskalenda3@gmail.com>
2022-02-28 15:40:02 +0200

    call app vieew in the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
-import NavBar from "./components/NavBar";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Missions from './components/missions/missions';
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
-    </>
+      <Routes>
+        <Route path="/missions" element={<Missions />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
