559c23c03fdc42b995c79da011338ac8c8e352a0
mugisha Samuel <mugishasamuel42@gmail.com>
2022-02-28 03:02:36 -0800

    Add empty navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import React from "react";
+import NavBar from "./components/NavBar";
 function App() {
-  return(<h1>hello</h1>)
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
 
 export default App;
