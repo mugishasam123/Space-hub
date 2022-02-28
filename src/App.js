1209c5c22c49f789e7aa3d6a4f66bb0c43a40c28
joskalenda <joskalenda3@gmail.com>
2022-02-28 18:37:23 +0200

    Fix linter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MissionPage from './components/missions/MissionPage';
-import NavBar from "./components/NavBar";
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
-    
+
       <Router>
-      <NavBar />
+        <NavBar />
         <Routes>
-          <Route path="/" element={<h1>hello world</h1>}/>
-          <Route path="/profile" element={
-            <h1>hello pro</h1>
-          }/>
+          <Route path="/" element={<h1>hello world</h1>} />
+          <Route
+            path="/profile"
+            element={
+              <h1>hello pro</h1>
+          }
+          />
           <Route path="/missions" element={<MissionPage />} />
         </Routes>
       </Router>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from './logo.png';
+import './styles/navbar.css';
 
-import React from "react";
-import { NavLink } from "react-router-dom";
-import logo from './logo.png'
-import './styles/navbar.css'
-
-const NavBar = () => {
-  return (
-    <>
-      <div className="navbar">
-        <img src={logo} alt="" className="logo" />
-        <NavLink to="/profile">My Profile</NavLink>
-        <NavLink to="/missions">Missions</NavLink>
-      </div>
-    </>
-  );
-};
+const NavBar = () => (
+  <>
+    <div className="navbar">
+      <img src={logo} alt="" className="logo" />
+      <NavLink to="/profile">My Profile</NavLink>
+      <NavLink to="/missions">Missions</NavLink>
+    </div>
+  </>
+);
 export default NavBar;
