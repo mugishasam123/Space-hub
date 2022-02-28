da1f008656fa285b492aa975fc702d8cae2b9e61
joskalenda <joskalenda3@gmail.com>
2022-02-28 15:38:57 +0200

    Create view component for the mission page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+const NavBar = () => (
+  <nav>
+    <Link to="/missions">Missions</Link>
+  </nav>
+);
 
 export default NavBar;
