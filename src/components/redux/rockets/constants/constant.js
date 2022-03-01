4e20c6fec274dc4f873af6543bf014bdad06d66c
mugisha Samuel <mugishasamuel42@gmail.com>
2022-03-01 07:04:26 -0800

    Solve conflicts

--- src/components/redux/rockets/constants/constant.js
+++ src/components/redux/rockets/constants/constant.js
@@ -1,1 +1,3 @@
 export const ADD_ROCKETS = "spacehub/rockets/ADD_ROCKETS";
+export const RESERVE_ROCKET = "spacehub/rockets/ADD_ROCKETS";
+export const CANCEL_ROCKETS = "spacehub/rockets/ADD_ROCKETS";
