1d8580d0b471e6746d11a2f1d0a62fe0ae9ae92c
joskalenda <joskalenda3@gmail.com>
2022-03-02 12:04:26 +0200

    Create mission reducer store

--- src/components/redux/rockets/store/configureStore.js
+++ src/components/redux/rockets/store/configureStore.js
@@ -2,7 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import { rocketReducer } from "../reducers/rocketReducer";
-
-const rootReducer = combineReducers({rockets:rocketReducer,})
+import { missionReducer } from '../../../../redux/missions/missionActions'
+const rootReducer = combineReducers({
+  rockets:rocketReducer,
+  missions:missionReducer,
+})
 
 export const Store = createStore(rootReducer,applyMiddleware(thunk,logger));
