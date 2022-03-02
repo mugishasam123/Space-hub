f68a634937e027e55dfb3ed1dfe096150c86247f
joskalenda <joskalenda3@gmail.com>
2022-03-02 16:25:02 +0200

    Display missons to the profile secton

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,25 +1,41 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import DisplayContainer from "./missions/DisplayMission";
+import  { getReservedMissions } from '../components/missions/getMissions';
 import "./styles/profile.css";
 
 const Profile = () => {
+  const ProfileContainer = {
+      display: 'flex',
+      justifyContent: 'space-between',
+      margin: '0 2rem',
+      gap: '2rem',
+      flexGrow: 1,}
+
+  const reservedMission = useSelector(getReservedMissions);
   const data = useSelector((state) => state.rockets);
   const newData = data.filter((rocket) => rocket.reserved === true);
   return (
-    <div className="reserved-section">
-      <h3 className="reserved-title">My Rockets</h3>
-      {newData ? (
-         <div className="reserved-rockets">
-         {newData.map((rocket) => (
-           <span key={rocket.id} className="reserved-rocket">
-             {rocket.name}
-           </span>
-         ))}
-       </div>
-        
-      ) : (
-        <li>No Reserved Rockets to display</li>
-      )}
+    <div style={ProfileContainer}>
+      <DisplayContainer
+        title="My Missions"
+        data={reservedMission}
+      />
+      <div className="reserved-section">
+        <h3 className="reserved-title">My Rockets</h3>
+        {newData ? (
+          <div className="reserved-rockets">
+          {newData.map((rocket) => (
+            <span key={rocket.id} className="reserved-rocket">
+              {rocket.name}
+            </span>
+          ))}
+        </div>
+          
+        ) : (
+          <li>No Reserved Rockets to display</li>
+        )}
+      </div>
     </div>
   );
 };
